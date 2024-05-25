089d8dcd15178cc562993cfab0a523abbb1c1f1e
imbagila <roid.rabbani@gmail.com>
2024-05-25 07:14:55 +0700

    feat: update project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,14 +7,29 @@ export type Project = {
 
 const projects: Project[] = [
   {
+    title: "Kantonq • Simple Money Management",
+    techs: ["Financial", "SvelteKit", "Go"],
+    link: "https://github.com/imbagila/kantonq",
+  },
+  {
     title: "Jingu • Kong Admin UI",
-    techs: ["Kong", "SvelteKit", "Rust", "Tauri"],
+    techs: ["Kong", "SvelteKit", "Rust"],
     link: "https://github.com/imbagila/jingu",
   },
   {
     title: "Treant • Mongo Shard Visualizer",
-    techs: ["Mongo", "SvelteKit", "Rust", "Tauri"],
-    link: "/",
+    techs: ["Mongo", "SvelteKit", "Rust"],
+    link: "https://github.com/imbagila/treant",
+  },
+  {
+    title: "docusaurus-decentralized-docs",
+    techs: ["Library", "Docusaurus", "TypeScript"],
+    link: "https://github.com/imbagila/docusaurus-decentralized-docs",
+  },
+  {
+    title: "Zero • Go Project Boilerplate",
+    techs: ["Go"],
+    link: "https://github.com/imbagila/zero",
     isComingSoon: true,
   },
 ];
